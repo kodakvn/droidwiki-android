34fc7754f144674cdb2722b58b6ab68034c8aa09
KODAK <linhtt.it@gmail.com>
2020-02-25 03:54:50 +0700

    setup dagger for Application

---
+++ app/src/main/java/com/raywenderlich/android/droidwiki/dagger/AppComponent.kt
@@ -0,0 +1,8 @@
+package com.raywenderlich.android.droidwiki.dagger
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent
